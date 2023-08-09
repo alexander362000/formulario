5099394d7b9eac859de97797fc60055556acbc48
Alexander <alexander3.6.2000@gmail.com>
2023-08-09 18:50:25 -0300

    primer cambio

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const boton = document.getElementById("regBtn");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+boton.addEventListener("click", function() {
+    if (password1.value === password2.value) {
+        showAlertSuccess()
+      }
+      else{
+        showAlertError()
+      }
+});
